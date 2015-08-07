dc35f166f80708fc031d234f897921fb18f61963
rScientist <rScientist@users.noreply.github.com>
2015-08-06 23:51:35 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ source_dir <- "./household_power_consumption.txt"
 source_data <- read.table(source_dir, header = TRUE, sep = ";", stringsAsFactors=FALSE)
 feb_data <- source_data[source_data$Date %in% c("1/2/2007", "2/2/2007") ,]
 global_active_power <- as.numeric(feb_data$Global_active_power)
-png("Plot1.png", width = 480, height = 480)
+png("plot1.png", width = 480, height = 480)
 hist(global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
 dev.off()
 
